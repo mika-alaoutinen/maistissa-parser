239779e95ccbcce0e0839acbe6c317093f4a3b95
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-05-27 17:00:44 +0300

    Update Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,5 @@
 module Main where
 
-import Data.Maybe (fromMaybe)
-import WineParser (WineProperty (Name), parseName)
-
-wineName = "VIINI: Apothic Dark 2015"
-
 main :: IO ()
 main = do
-  let (Name name) = fromMaybe (Name "No name") $ parseName wineName
-  putStrLn $ "Name: " ++ name
+  putStrLn "WIP Parser combinators"
