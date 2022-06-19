ced8b883dd0356e422d75ac228394cb90751c655
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-06-19 17:14:29 +0300

    priceParser should work but doesn't for some bizarre reason

--- src/WineParser.hs
+++ src/WineParser.hs
@@ -2,7 +2,8 @@ module WineParser where
 
 import Control.Applicative (Alternative ((<|>)))
 import Parser.Combinators (separatedBy)
-import Parser.Parser (Parser)
+import Parser.Parser (Parser (..))
+import Parser.Predicates.Digits (double)
 import Parser.Predicates.Lines (newline, withPrefix)
 
 data WineProperty
@@ -14,7 +15,7 @@ data WineProperty
   | Url (Maybe String)
   deriving (Show, Eq)
 
-testStr = "VIINI: Apothic Dark 2015\nMaa: Espanja"
+testStr = "VIINI: Apothic Dark 2015\nMaa: Espanja\nHinta: 13.49"
 
 nameParser :: Parser WineProperty
 nameParser = Name <$> withPrefix "VIINI"
@@ -22,8 +23,11 @@ nameParser = Name <$> withPrefix "VIINI"
 countryParser :: Parser WineProperty
 countryParser = Country <$> withPrefix "Maa"
 
+priceParser :: Parser WineProperty
+priceParser = Price <$> (withPrefix "Hinta" *> double)
+
 winePropertyParser :: Parser WineProperty
-winePropertyParser = nameParser <|> countryParser
+winePropertyParser = nameParser <|> countryParser <|> priceParser
 
 parseWineProperties :: Parser [WineProperty]
 parseWineProperties = winePropertyParser `separatedBy` newline
