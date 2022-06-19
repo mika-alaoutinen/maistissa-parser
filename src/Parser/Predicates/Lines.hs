2e2038a8c0fbc141282696fdbb4304da4196bd58
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2022-06-19 19:26:33 +0300

    Fix wineparser (#3)
    Change withPrefix function to also take a parser function as predicate

--- src/Parser/Predicates/Lines.hs
+++ src/Parser/Predicates/Lines.hs
@@ -3,13 +3,13 @@ module Parser.Predicates.Lines (newline, withPrefix) where
 import Control.Applicative ((<|>))
 import Parser.Parser (Parser)
 import Parser.Predicates.Chars (char)
-import Parser.Predicates.Strings (anyString, spaces, string)
+import Parser.Predicates.Strings (spaces, string)
 
 newline :: Parser Char
 newline = char '\n' <|> (char '\r' *> char '\n')
 
-withPrefix :: String -> Parser String
-withPrefix prefix = string prefix <* stripColon *> anyString
+withPrefix :: String -> Parser a -> Parser a
+withPrefix prefix predicate = string prefix <* stripColon *> predicate
 
 -- Helpers
 stripColon :: Parser Char
