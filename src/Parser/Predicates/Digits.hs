a44653af80c6a7ffeb5e92a7b89eec8e0f870477
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-06-19 15:51:19 +0300

    WIP digits

--- src/Parser/Predicates/Digits.hs
+++ src/Parser/Predicates/Digits.hs
@@ -2,7 +2,8 @@ module Parser.Predicates.Digits where
 
 import Control.Applicative (Alternative (..))
 import Data.Char (digitToInt, isDigit)
-import Parser.Parser (Parser, satisfy)
+import Parser.Parser (Parser (..), satisfy)
+import Parser.Predicates.Chars (char)
 
 digit :: Parser Int
 digit = digitToInt <$> satisfy isDigit
@@ -10,7 +11,21 @@ digit = digitToInt <$> satisfy isDigit
 digits :: Parser [Int]
 digits = many digit
 
--- anyDecimal :: Parser String
--- anyDecimal = digits <* decimalPoint <* digits
---   where
---     decimalPoint = char '.' <|> char ','
+integer :: Parser Int
+integer = fromDigits <$> digits
+
+anyDecimal :: Parser [Int]
+anyDecimal = digits <* decimalPoint <* digits
+  where
+    decimalPoint = char '.' <|> char ','
+
+-- Helpers
+fractional :: Parser [Int]
+fractional = decimalPoint *> digits
+  where
+    decimalPoint = char '.' <|> char ','
+
+fromDigits :: [Int] -> Int
+fromDigits = foldl addDigit 0
+  where
+    addDigit num d = 10 * num + d
