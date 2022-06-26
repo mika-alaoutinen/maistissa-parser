b43e423f5f646b5e3c68f2aae334ae38f5ba286e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-06-26 18:10:58 +0300

    Add urlParser

--- src/Model/Wine.hs
+++ src/Model/Wine.hs
@@ -3,7 +3,9 @@ module Model.Wine where
 data Wine = Wine
   { name :: String,
     country :: String,
+    price :: Double,
     description :: [String],
-    foodPairings :: [String]
+    foodPairings :: [String],
+    url :: Maybe String
   }
   deriving (Show, Eq)
--- src/WineParser.hs
+++ src/WineParser.hs
@@ -1,6 +1,7 @@
 module WineParser where
 
 import Control.Applicative (Alternative ((<|>)))
+import Model.Wine (Wine (..))
 import Parser.Combinators (separatedBy)
 import Parser.Lines (parseDouble, parseString, parseStrings)
 import Parser.Parser (Parser (..))
@@ -20,7 +21,7 @@ testStr =
   \Maa: Espanja\n\
   \Hinta: 13.49\n\
   \Kuvaus: Pehmeä ja hedelmäinen, täyteläinen\n\
-  \SopiiNautittavaksi: seurustelujuomana, pikkusuolaiset, pasta ja pizza, grilliruoka"
+  \SopiiNautittavaksi: seurustelujuomana, pikkusuolaiset, pasta ja pizza, grilliruoka\n"
 
 -- Parse specific wine properties
 nameParser :: Parser WineProperty
@@ -38,9 +39,37 @@ descriptionParser = Description <$> parseStrings "Kuvaus"
 foodPairingsParser :: Parser WineProperty
 foodPairingsParser = FoodPairings <$> parseStrings "SopiiNautittavaksi"
 
+urlParser :: Parser WineProperty
+urlParser = Url . Just <$> parseString "url"
+
 -- Parse wine properties
 winePropertyParser :: Parser WineProperty
-winePropertyParser = nameParser <|> countryParser <|> priceParser <|> descriptionParser <|> foodPairingsParser
+winePropertyParser =
+  nameParser
+    <|> countryParser
+    <|> priceParser
+    <|> descriptionParser
+    <|> foodPairingsParser
+    <|> urlParser
 
-parseWineProperties :: Parser [WineProperty]
-parseWineProperties = winePropertyParser `separatedBy` newline
+winePropertiesParser :: Parser [WineProperty]
+winePropertiesParser = winePropertyParser `separatedBy` newline
+
+parseWine :: String -> Maybe Wine
+parseWine input = do
+  (Name name, unparsed1) <- runParser winePropertyParser input
+  (Country country, unparsed2) <- runParser winePropertyParser unparsed1
+  (Price price, unparsed3) <- runParser winePropertyParser unparsed2
+  (Description description, unparsed4) <- runParser winePropertyParser unparsed3
+  (FoodPairings foodPairings, unparsed5) <- runParser winePropertyParser unparsed4
+  (Url url, unparsed6) <- runParser winePropertyParser unparsed5
+
+  Just
+    Wine
+      { name = name,
+        country = country,
+        price = price,
+        description = description,
+        foodPairings = foodPairings,
+        url = url
+      }
