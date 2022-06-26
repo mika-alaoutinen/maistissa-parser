3839c47f025de56bde87930ed6dc20c0e230c9ef
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-06-26 18:27:23 +0300

    Parsing a wine works with happy case

--- src/WineParser.hs
+++ src/WineParser.hs
@@ -6,6 +6,8 @@ import Parser.Combinators (separatedBy)
 import Parser.Lines (parseDouble, parseString, parseStrings)
 import Parser.Parser (Parser (..))
 import Parser.Predicates.Chars (newline)
+import Debug.Trace
+import Data.Maybe (fromMaybe)
 
 data WineProperty
   = Name String
@@ -21,7 +23,8 @@ testStr =
   \Maa: Espanja\n\
   \Hinta: 13.49\n\
   \Kuvaus: Pehmeä ja hedelmäinen, täyteläinen\n\
-  \SopiiNautittavaksi: seurustelujuomana, pikkusuolaiset, pasta ja pizza, grilliruoka\n"
+  \SopiiNautittavaksi: seurustelujuomana, pikkusuolaiset, pasta ja pizza, grilliruoka\n\
+  \url: https://alko.fi/123"
 
 -- Parse specific wine properties
 nameParser :: Parser WineProperty
@@ -55,14 +58,23 @@ winePropertyParser =
 winePropertiesParser :: Parser [WineProperty]
 winePropertiesParser = winePropertyParser `separatedBy` newline
 
+-- Parsinta epäonnistuu, koska viinin nimen jälkeen unparsed1-merkkijonon alussa on rivinvaihto.
+-- Rivinvaihto täytyy joko poistaa tai keksiä jokin tapa parsia jokainen rivi `separatedBy` \n.
 parseWine :: String -> Maybe Wine
 parseWine input = do
-  (Name name, unparsed1) <- runParser winePropertyParser input
-  (Country country, unparsed2) <- runParser winePropertyParser unparsed1
-  (Price price, unparsed3) <- runParser winePropertyParser unparsed2
-  (Description description, unparsed4) <- runParser winePropertyParser unparsed3
-  (FoodPairings foodPairings, unparsed5) <- runParser winePropertyParser unparsed4
-  (Url url, unparsed6) <- runParser winePropertyParser unparsed5
+  (Name name, unparsed1) <- runParser winePropertyParser (dropLineBreak input)
+  (Country country, unparsed2) <- runParser winePropertyParser (dropLineBreak unparsed1)
+  (Price price, unparsed3) <- runParser winePropertyParser (dropLineBreak unparsed2)
+  (Description description, unparsed4) <- runParser winePropertyParser (dropLineBreak unparsed3)
+  (FoodPairings foodPairings, unparsed5) <- runParser winePropertyParser (dropLineBreak unparsed4)
+  (Url url, unparsed6) <- runParser winePropertyParser (dropLineBreak unparsed5)
+
+  trace ("Name: " ++ name) Just name
+  trace ("Country: " ++ country) Just country
+  trace ("Price: " ++ show price) Just price
+  trace ("Description: " ++ show (length description)) Just description
+  trace ("Food Pairings: " ++ show (length foodPairings)) Just foodPairings
+  trace ("url: " ++ fromMaybe "no url" url) url
 
   Just
     Wine
@@ -73,3 +85,6 @@ parseWine input = do
         foodPairings = foodPairings,
         url = url
       }
+
+dropLineBreak :: String -> String
+dropLineBreak = dropWhile (== '\n')
