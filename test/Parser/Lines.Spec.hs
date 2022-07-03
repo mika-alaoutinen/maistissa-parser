5868845130b2604b9c43f5112529a1c620701095
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-07-03 16:18:57 +0300

    WIP Lines.Spec

---
+++ test/Parser/Lines.Spec.hs
@@ -0,0 +1,19 @@
+import Parser.Lines (parseString)
+import Parser.Parser (Parser (runParser))
+import Test.QuickCheck
+
+runTests :: IO ()
+runTests = do
+  quickCheck prop_parseString
+
+prop_parseString :: String -> Property
+prop_parseString prefix = forAll alphabetString $ \str -> case parse (input str) of
+  Nothing -> False
+  Just (parsed, _) -> parsed == str
+  where
+    parse = runParser $ parseString prefix
+    input value = prefix ++ ": " ++ value
+
+-- Helpers
+alphabetString :: Gen String
+alphabetString = listOf1 (elements ['a' .. 'z'])
