f974f9b3d7b3a2b907126396b1b4dccc641f1c3e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-07-10 11:24:18 +0300

    Tests for parsing many wine properties

--- test/Parser/WinePropertiesSpec.hs
+++ test/Parser/WinePropertiesSpec.hs
@@ -1,5 +1,6 @@
 module Parser.WinePropertiesSpec (spec) where
 
+import Data.Maybe (fromMaybe)
 import Parser.Parser (Parser (runParser))
 import Parser.WineProperties
 import Test.Hspec
@@ -22,6 +23,12 @@ spec = do
     it "should parse URL" $ do
       parseProp "url: http://viini.fi/123" `shouldBe` Just (Url $ Just "http://viini.fi/123", "")
 
+  describe "Parses many wine properties that are separeted by newline" $ do
+    it "should parse all wine properties" $ do
+      fromMaybe 0 parsedPropertiesCount `shouldBe` 6
+    it "should parse wine name and stop on invalid line" $ do
+      parseProps invalidWineEntry `shouldBe` Just ([Name "Apothic Dark 2015"], "\nabc")
+
   describe "Parses Nothing on invalid input" $ do
     it "should return Nothing on incorrect prefix" $ do
       parseProp "invalid: Apothic Dark 2015" `shouldBe` Nothing
@@ -29,5 +36,23 @@ spec = do
       parseProp "Maa: " `shouldBe` Nothing
 
 -- Helpers
+wineEntry =
+  "VIINI: Apothic Dark 2015\n\
+  \Maa: Espanja\n\
+  \Hinta: 13.49\n\
+  \Kuvaus: Pehmeä ja hedelmäinen, täyteläinen\n\
+  \SopiiNautittavaksi: seurustelujuomana, pikkusuolaiset, pasta ja pizza, grilliruoka\n\
+  \url: https://alko.fi/123"
+
+invalidWineEntry = "VIINI: Apothic Dark 2015\nabc"
+
 parseProp :: String -> Maybe (WineProperty, String)
 parseProp = runParser winePropertyParser
+
+parseProps :: String -> Maybe ([WineProperty], String)
+parseProps = runParser winePropertiesParser
+
+parsedPropertiesCount :: Maybe Int
+parsedPropertiesCount = do
+  (parsedProperties, _) <- parseProps wineEntry
+  return $ length parsedProperties
