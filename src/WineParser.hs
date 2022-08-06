75c0f0ac5626a8a0e14315866776fe9db6fc98bf
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-08-06 14:35:50 +0300

    WIP parse wine from properties

--- src/WineParser.hs
+++ src/WineParser.hs
@@ -3,13 +3,31 @@
 module WineParser (parseWine) where
 
 import Model.Wine (Wine (..))
+import Parser.Combinators (separatedBy)
 import Parser.Parser (Error (..), Parser (..))
+import Parser.Predicates.Chars (newline)
 import Parser.WineProperties (WineProperty (..), winePropertiesParser)
 
 parseWine :: String -> Maybe Wine
 parseWine input = case runParser winePropertiesParser input of
   Right (wineProperties, _) -> mkWine wineProperties
   Left _ -> Nothing
+
+parseWines :: String -> [Wine]
+parseWines input = case runParser winesParser input of
+  Right (wines, _) -> wines
+  Left errors -> []
+
+wineParser :: Parser Wine
+wineParser = wineFromProperties winePropertiesParser
+
+winesParser :: Parser [Wine]
+winesParser = wineParser `separatedBy` newline
+
+wineFromProperties :: Parser [WineProperty] -> Parser Wine
+wineFromProperties properties = undefined
+  where
+    x = mkWine <$> properties
 
 -- Helpers
 mkWine :: [WineProperty] -> Maybe Wine
